06effd4c917d1f623fab1da6af55643544b35adf
warbest101 <ttphuc75@gmail.com>
2022-03-15 08:48:07 +0700

    CS-15: Init project

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use Auth;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('login');
+        }
+    }
+
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if (! $request->expectsJson()) {
+            return response("Not Authenticated.");
+        }
+        return $next($request);
+    }
+}
